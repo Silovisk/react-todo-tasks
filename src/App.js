5865b6ce3dd2a4a43f5fad08343e32087719f616
Silovisk <silasaraujo1@hotmail.com>
2023-07-28 09:48:55 -0300

    Update todo

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ const API = "http://localhost:5000";
 function App() {
   const [title, setTitle] = useState("");
   const [time, setTime] = useState("");
+  const [comment, setComment] = useState("");
+
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(false);
-
 
   useEffect(() => {
     const loadData = async () => {
@@ -22,13 +23,11 @@ function App() {
         .catch((err) => console.log(err));
 
       setLoading(false);
-
       setTodos(res);
     };
 
     loadData();
-  }, [])
-
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -37,33 +36,33 @@ function App() {
       id: Math.random(),
       title,
       time,
+      comment,
       done: false,
     };
 
-    await fetch(API + "/todos",
-      {
-        method: "POST",
-        body: JSON.stringify(todoData),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+    await fetch(API + "/todos", {
+      method: "POST",
+      body: JSON.stringify(todoData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
     console.log(todoData);
 
     setTodos((prevState) => [...prevState, todoData]);
-    setTitle('');
-    setTime('');
+    setTitle("");
+    setTime("");
+    setComment("");
   };
 
   const handelDelete = async (id) => {
-
     await fetch(API + "/todos/" + id, {
       method: "DELETE",
     });
 
     setTodos((prevState) => prevState.filter((todo) => todo.id !== id));
-  }
+  };
 
   const handleEdit = async (todoData) => {
     todoData.done = !todoData.done;
@@ -79,7 +78,7 @@ function App() {
     setTodos((prevState) =>
       prevState.map((t) => (t.id === data.id ? (t = data) : t))
     );
-  }
+  };
 
   if (loading) {
     return <p>Carregando Pagina...</p>;
@@ -87,21 +86,24 @@ function App() {
 
   return (
     <div className="App">
-      <nav class="navbar navbar-expand-lg">
-        <div class="container-md d-flex justify-content-center text-center">
-          <a class="navbar-brand fw-bold fs-1" href="#">To-do List</a>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-md d-flex justify-content-center text-center">
+          <a className="navbar-brand fw-bold fs-1" href="#">
+
+          </a>
         </div>
       </nav>
 
       <div className="container">
         <div className="bg-todo-color d-felx justify-content-center align-items-center text-center w-50 mx-auto p-5">
           <div className="todo-header">
-
+            <h2 className="fw-bold fs-3">Criar Tarefa</h2>
             <hr className="" />
           </div>
 
           <div className="form-todo">
             <form className="" onSubmit={handleSubmit}>
+
               <div className="form-floating mb-3">
                 <input
                   type="text"
@@ -130,40 +132,63 @@ function App() {
               </div>
 
               <div className="form-floating">
-                <textarea className="form-control" placeholder="Escreva um comentario" id="TextareaComment"></textarea>
-                {/* style="height: 100px" */}
+                <textarea
+                  className="form-control"
+                  placeholder="Escreva um comentario"
+                  id="TextareaComment"
+                  onChange={(e) => setComment(e.target.value)}
+                  value={comment || ""}
+                  required
+                ></textarea>
+
                 <label for="TextareaComment">Comentario</label>
               </div>
 
-              <button className="btn btn-success my-4 bg-button-addTodo" type="submit" value="Criar Tarefa">
+              <button
+                className="btn btn-success my-4 bg-button-addTodo"
+                type="submit"
+                value="Criar Tarefa"
+              >
                 Criar Tarefa
               </button>
             </form>
           </div>
 
-          <hr class="border border-primary border-3 opacity-75" />
+          <hr className="border border-primary border-3 opacity-75" />
 
           <div className="list-todo">
-          <h2>Lista de Tarefas</h2>
-          <hr class="border border-primary border-3 opacity-75" />
-            
-            {todos.length === 0 && <p>Não há Tarefas!</p>}
-            {todos.map((todo) => (
-              <div className="todo" key={todo.id}>
-                <h3 className={todo.done ? "todo-done" : ""}>{todo.title}</h3>
-                <p>Duração: {todo.time}</p>
-                <div className="">
-                  <span onClick={() => handleEdit(todo)}>{!todo.done ? <BsBookmarkCheck /> : <BsBookmarkCheckFill />}</span>
-
-                  <BsTrash onClick={() => handelDelete(todo.id)} />
-
-                </div>
-
-                <hr class="border border-danger border-2 opacity-50"/>
-
-              </div>
-            ))}
-
+            <h2>Lista de Tarefas</h2>
+            <hr className="border border-primary border-3 opacity-75" />
+            <table className="table table-todo table-hover">
+              <thead>
+                <tr className="fs-5 fw-bold">
+                  <th scope="col">Titulo</th>
+                  <th scope="col">Duração</th>
+                  <th scope="col">Commentario</th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+              <tbody className="table-group-divider">
+                {todos.length === 0 && <p>Não há Tarefas!</p>}
+                {todos.map((todo) => (
+                  <tr className="todo" key={todo.id}>
+                    <th scope="row"><h4 className={todo.done ? "todo-done" : ""}>{todo.title}</h4></th>
+                    <td>{todo.time}</td>
+                    <td>{todo.comment}</td>
+                    <td>
+                      <div className="">
+                        <span className="bs-bookmark" onClick={() => handleEdit(todo)}>
+                          {!todo.done ? <BsBookmarkCheck /> : <BsBookmarkCheckFill />}
+                        </span>
+                        <span className="bs-trash mx-1">
+                          <BsTrash onClick={() => handelDelete(todo.id)} />
+                        </span>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
